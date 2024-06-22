badd3d159c6bf1f4554e38645c6764f69ab67a2e
scx1332 <sieciech.czajka@golem.network>
2024-06-22 16:00:19 +0200

    Prepare sources

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import * as core from "@actions/core";
+import * as installer from "./installer";
+
+async function run() {
+  try {
+    const version = core.getInput("version");
+    const includePreReleases = convertToBoolean(
+      core.getInput("include-pre-releases"),
+    );
+    const repoToken = core.getInput("repo-token");
+    await installer.getProtoc(version, includePreReleases, repoToken);
+  } catch (error) {
+    core.setFailed(`${error}`);
+  }
+}
+
+run();
