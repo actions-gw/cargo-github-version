526590d8cdc7fa0fcd602bf15a16304d278c718e
scx1332 <sieciech.czajka@golem.network>
2024-06-22 16:31:30 +0200

    working on action

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,34 @@
 import * as core from "@actions/core";
 
+
+function extractSemver(ref) {
+  const semverRegex = /v?(\d+\.\d+\.\d+)/;
+  const match = ref.match(semverRegex);
+  return match ? match[1] : null;
+}
+
+
 async function run() {
   try {
     const prefix = core.getInput("prefix");
     const suffix = core.getInput("suffix");
 
-    const targetVersion = "1.2.3";
-    core.setOutput("version", targetVersion);
-    core.setOutput("version-ext", targetVersion + suffix);
-    core.setOutput("version-full", prefix + targetVersion + suffix);
+    // extract version from github ref
 
-    console.log(`Version: ${targetVersion}`);
+    // refs/tags/v1.2.3
+
+    const version = extractSemver(process.env.GITHUB_REF);
+    if (!version) {
+      throw new Error(`Failed to extract version from GITHUB_REF: ${process.env.GITHUB_REF}`);
+    }
+
+    console.log(`version=${version}`);
+    console.log(`version-ext=${version + suffix}`);
+    console.log(`version-full=${prefix + version + suffix}`);
+
+    core.setOutput("version", version);
+    core.setOutput("version-ext", version + suffix);
+    core.setOutput("version-full", prefix + version + suffix);
 
   } catch (error) {
     core.setFailed(`${error}`);
